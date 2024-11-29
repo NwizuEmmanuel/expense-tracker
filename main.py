229ff838eb8d84ada296344467c4d7bb73f3734b
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-11-29 19:08:03 +0800

    function: list expenses

--- main.py
+++ main.py
@@ -8,14 +8,23 @@ def check_db():
             writer = csv.DictWriter(file, fieldnames=["id","description","amount"])
             writer.writeheader()
 
-def save_to_db(data):
-    with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file)
-        writer.writerow()
+def save_to_db(data: dict):
+    fieldnames = list(data.keys())
+    with open(filename, 'a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(data)
 
-def add_expense(args):
+def give_index():
+    index = 1
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            index += 1
+    return index
+    
+def add_expense(args=None):
     data = {
-        'id': args.id,
+        'id': give_index(),
         'description': args.description,
         'amount': args.amount
     }
@@ -24,22 +33,34 @@ def add_expense(args):
     print(f"{data['id']}. {data['description']}={data['amount']}")
 
 
+def list_expense(args=None):
+    with open(filename, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            print(f"{row['id']}. {row['description']} => {row['amount']}")
+
 def main():
     check_db()
     parser = argparse.ArgumentParser(description="Expense Tracker.")
-    subparsers = parser.add_subparsers(help="Available commands")
+    subparsers = parser.add_subparsers(dest='command', help="Available commands")
 
-    add_parser = subparsers.add_parser("add", help="To add new expense.")
+    add_parser = subparsers.add_parser("add", help="Add new expense.")
     add_parser.add_argument("-d", "--description", required=True, help="Description of expense.")
     add_parser.add_argument("-a","--amount", type=int, required=True, help="Amount of expense.")
 
+    subparsers.add_parser("list", help="List expenses.") # for listing expenses.
+
+    delete_parser = subparsers.add_parser("delete", help="Delete an expense item")
+    delete_parser.add_argument("id", type=int, help="Expense item id")
+
     args = parser.parse_args()
     commands = {
-        "add":add_expense
+        "add":add_expense,
+        'list': list_expense,
     }
     for c in commands:
         if args.command == c:
-            commands[c]()
+            commands[c](args)
 
 if __name__ == "__main__":
     main()
