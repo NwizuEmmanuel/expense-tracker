938b56616165c69f5a228288e50c62b3ea678741
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-11-30 12:05:50 +0800

    Revert "function: list expenses"
    This reverts commit 229ff838eb8d84ada296344467c4d7bb73f3734b.
    
     You are currently reverting commit 229ff83.
    
     Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
-"""Manipulate csv files"""
+"""Expense tracker"""
 import csv
+import argparse
+import os
+
+FILENAME = "db.csv"
+
+def check_db():
+    """check for csv file"""
+    if not os.path.exists(FILENAME):
+        with open(FILENAME, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=["id","description","amount"])
+            writer.writeheader()
+
+def save_to_db(data):
+    with open(FILENAME, 'w', newline='') as file:
+        writer = csv.DictWriter(file)
+        writer.writerow()
+
+def add_expense(args):
+    data = {
+        'id': args.id,
+        'description': args.description,
+        'amount': args.amount
+    }
+    save_to_db(data=data)
+    print("saved")
+    print(f"{data['id']}. {data['description']}={data['amount']}")
 
 
-def read_file():
-    """
-    To read csv files
-    """
-    with open('sample.csv', mode='r', newline='', encoding='utf-8') as f:
-        csv_reader = csv.reader(f)
-        header = next(csv_reader)
-        print(f"Header: {header}")
+def main():
+    check_db()
+    parser = argparse.ArgumentParser(description="Expense Tracker.")
+    subparsers = parser.add_subparsers(help="Available commands")
 
-        for row in csv_reader:
-            print(row)
+    add_parser = subparsers.add_parser("add", help="To add new expense.")
+    add_parser.add_argument("-d", "--description", required=True, help="Description of expense.")
+    add_parser.add_argument("-a","--amount", type=int, required=True, help="Amount of expense.")
 
+    args = parser.parse_args()
+    commands = {
+        "add":add_expense
+    }
+    for c in commands:
+        if args.command == c:
+            commands[c]()
 
-def add_to_file():
-    """Add to csv file"""
-    with open('sample.csv', mode='a', newline='', encoding='utf-8') as f:
-        data = ['6', 'Spinach', '34.3']
-        csv_writer = csv.writer(f)
-        csv_writer.writerow(data)
-
-
-def delete_from_file(index):
-    """Delete from csv files"""
-    with open('sample.csv', newline='', mode='r', encoding='utf-8') as infile:
-        csv_reader = csv.reader(infile)
-        header = next(csv_reader)
-        rows = [row for row in csv_reader if int(row[0]) != index]
-
-    with open('sample.csv', mode='w', newline='', encoding='utf-8') as outfile:
-        csv_writer = csv.writer(outfile)
-        csv_writer.writerow(header)
-        csv_writer.writerows(rows)
-    print(f"item {index} is delete.")
-
-
-if __name__ == "__main__":
-    read_file()
-    delete_from_file(3)
-    read_file()
