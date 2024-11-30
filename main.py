87286ad0a7b13d9aa860613cad2a19c3554a07bd
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-11-30 18:40:06 +0800

    Added app functions: list, summary, delete

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """Expense tracker"""
+
+from datetime import datetime
 import csv
 import argparse
 import os
 
 FILENAME = "db.csv"
+HEADERS = ['id', 'description', 'amount','date']
 
 
 def check_db():
@@ -14,14 +17,18 @@ def check_db():
     if not os.path.exists(FILENAME):
         with open(FILENAME, 'a', newline='', encoding='utf-8') as file:
             csv_writer = csv.writer(file)
-            csv_writer.writerow(['id', 'description', 'amount'])
+            csv_writer.writerow(HEADERS)
 
 
-def write_to_db(data, mode):
+def write_to_db(data, mode, header=None):
     """save data to csv"""
     with open(FILENAME, mode, newline='', encoding='utf-8') as file:
         csv_writer = csv.writer(file)
-        csv_writer.writerow(data)
+        if mode == 'w':
+            csv_writer.writerow(header)
+            csv_writer.writerows(data)
+        else:
+            csv_writer.writerow(data)
 
 
 def read_db():
@@ -49,14 +56,17 @@ def get_id():
 
 def add_expense(args):
     """Expense function"""
+    now = datetime.now()
+    now = now.strftime("%d/%m/%Y")
     data = [
         get_id(),
         args.description,
-        args.amount
+        args.amount,
+        now
     ]
     write_to_db(data=data, mode='a')
     print("saved")
-    print(f"{data[0]}. {data[1]}={data[2]}")
+    print(f"{data[0]}. {data[1]}={data[2]} ({data[3]})")
 
 
 def list_expense(_):
@@ -66,7 +76,24 @@ def list_expense(_):
 
 
 def delete_expense(args):
-    pass
+    """delete expense"""
+    new_data = [item for item in read_db()['data'] if int(item[0]) != args.id]
+    write_to_db(data=new_data, mode='w', header=HEADERS)
+    print(f"Expense delete: {args.id}")
+
+
+def summary(args):
+    """To get the summary"""
+    data = read_db()["data"]
+    result = 0
+    for item in data:
+        result += float(item[2])
+
+    if args.month > 0:
+        pass
+    print(f"Total: {result}")
+
+
 def main():
     """main func"""
     check_db()
@@ -79,12 +106,21 @@ def main():
                             required=True, help="Description of expense.")
     add_parser.add_argument("-a", "--amount", type=float,
                             required=True, help="Amount of expense.")
-    subparsers.add_parser("list", help="List expenses")
+
+    _ = subparsers.add_parser("list", help="List expenses")
+
+    delete_parser = subparsers.add_parser("delete", help="To delete expense.")
+    delete_parser.add_argument("id", type=int, help="Expense ID to delete.")
+
+    summary_parser = subparsers.add_parser("summary", help="To summarize expense")
+    summary_parser.add_argument("--month", type=int, help="Filter with month.")
 
     args = parser.parse_args()
     commands = {
         "add": add_expense,
         "list": list_expense,
+        "delete": delete_expense,
+        'summary': summary
     }
     for key, value in commands.items():
         if args.command == key:
