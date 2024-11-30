088d7e8ba8f7b853f8af31b49e165dd79b9226d1
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-11-30 13:08:35 +0800

    function: list expense

--- main.py
+++ main.py
@@ -5,43 +5,91 @@ import os
 
 FILENAME = "db.csv"
 
+
 def check_db():
-    """check for csv file"""
+    """
+    check for csv file.
+    if not in existance will create one and prepare it.
+    """
     if not os.path.exists(FILENAME):
         with open(FILENAME, 'a', newline='', encoding='utf-8') as file:
-            writer = csv.DictWriter(file, fieldnames=["id","description","amount"])
-            writer.writeheader()
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(['id', 'description', 'amount'])
 
-def save_to_db(data):
-    with open(FILENAME, 'w', newline='') as file:
-        writer = csv.DictWriter(file)
-        writer.writerow()
+
+def write_to_db(data, mode):
+    """save data to csv"""
+    with open(FILENAME, mode, newline='', encoding='utf-8') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(data)
+
+
+def read_db():
+    """read and get db data"""
+    result = {
+        "header": [],
+        "data": [],
+    }
+    with open(FILENAME, mode='r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        result["header"] = next(csv_reader)
+        for item in csv_reader:
+            result['data'].append(item)
+    return result
+
+
+def get_id():
+    """
+    Get the total number of expense and add one to it.
+    prepare it for the new expense item.
+    """
+    index = len(read_db()["data"]) + 1
+    return index
+
 
 def add_expense(args):
-    data = {
-        'id': args.id,
-        'description': args.description,
-        'amount': args.amount
-    }
-    save_to_db(data=data)
+    """Expense function"""
+    data = [
+        get_id(),
+        args.description,
+        args.amount
+    ]
+    write_to_db(data=data, mode='a')
     print("saved")
-    print(f"{data['id']}. {data['description']}={data['amount']}")
+    print(f"{data[0]}. {data[1]}={data[2]}")
 
 
+def list_expense(_):
+    """List expense"""
+    for item in read_db()['data']:
+        print(f"{item[0]}. {item[1]}: {item[2]}")
+
+
+def delete_expense(args):
+    pass
 def main():
+    """main func"""
     check_db()
     parser = argparse.ArgumentParser(description="Expense Tracker.")
-    subparsers = parser.add_subparsers(help="Available commands")
+    subparsers = parser.add_subparsers(
+        dest="command", help="Available commands")
 
     add_parser = subparsers.add_parser("add", help="To add new expense.")
-    add_parser.add_argument("-d", "--description", required=True, help="Description of expense.")
-    add_parser.add_argument("-a","--amount", type=int, required=True, help="Amount of expense.")
+    add_parser.add_argument("-d", "--description",
+                            required=True, help="Description of expense.")
+    add_parser.add_argument("-a", "--amount", type=float,
+                            required=True, help="Amount of expense.")
+    subparsers.add_parser("list", help="List expenses")
 
     args = parser.parse_args()
     commands = {
-        "add":add_expense
+        "add": add_expense,
+        "list": list_expense,
     }
-    for c in commands:
-        if args.command == c:
-            commands[c]()
+    for key, value in commands.items():
+        if args.command == key:
+            value(args)
 
+
+if __name__ == "__main__":
+    main()
